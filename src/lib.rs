c8d0565bddd04a6822e5e8de301df8d2d0915247
Dyberg <mathias.rerup-dyberg@hotmail.com>
2024-10-10 16:19:23 +0200

    Added seq

--- src/lib.rs
+++ src/lib.rs
@@ -66,8 +66,22 @@ impl slang_ui::Hook for App {
 // a single assertion)
 fn cmd_to_ivlcmd(cmd: &Cmd) -> Result<IVLCmd> {
     match &cmd.kind {
+        CmdKind::Seq(cmd1, cmd2) => {
+            let ivl1 = cmd_to_ivlcmd(cmd1)?;
+            let ivl2 = cmd_to_ivlcmd(cmd2)?;
+            Ok(IVLCmd {
+                span: cmd.span.clone(),
+                kind: IVLCmdKind::Seq(Box::new(ivl1), Box::new(ivl2)),
+            })
+        },
         CmdKind::Assert { condition, .. } => Ok(IVLCmd::assert(condition, "Assert might fail!")),
-        CmdKind::VarDefinition { name, ty, expr } => Ok(IVLCmd::havoc(name, &ty.1)),
+        CmdKind::VarDefinition { name, ty, expr } => {
+            if let Some(expr) = expr {
+                Ok(IVLCmd::assign(name, expr))
+            } else {
+                Ok(IVLCmd::havoc(name, &ty.1))
+            }
+        },
         CmdKind::Assignment { name, expr } => Ok(IVLCmd::assign(name, expr)),
         _ => todo!("Not supported (yet). cmd_to_ivlcmd"),
     }
@@ -75,11 +89,16 @@ fn cmd_to_ivlcmd(cmd: &Cmd) -> Result<IVLCmd> {
 
 // Weakest precondition of (assert-only) IVL programs comprised of a single
 // assertion
-fn wp(ivl: &IVLCmd, postCondition: &Expr) -> Result<(Expr, String)> {
+fn wp(ivl: &IVLCmd, post_condition: &Expr) -> Result<(Expr, String)> {
     match &ivl.kind {
-        IVLCmdKind::Assert { condition, message } => Ok((condition.clone(), message.clone())),
-        IVLCmdKind::Havoc { name, ty } => Ok((postCondition.clone(), "Reason".to_string())),
-        IVLCmdKind::Assignment {expr, name} => Ok((postCondition.subst_ident(&name.ident, expr), "Reason for fail".to_string())),
+        IVLCmdKind::Seq(ivl1, ivl2) => {
+            let (wp2, msg2) = wp(ivl2, post_condition)?;
+            let (wp1, msg1) = wp(ivl1, &wp2)?;
+            Ok((wp1, format!("Msg1: {}, msg2: {}", msg1, msg2)))
+        },
+        IVLCmdKind::Assert { condition, message } => Ok((condition.clone() & post_condition.clone(), message.clone())),
+        IVLCmdKind::Havoc { name, ty } => Ok((post_condition.clone(), "Havoc".to_string())),
+        IVLCmdKind::Assignment {expr, name} => Ok((post_condition.subst_ident(&name.ident, expr), format!("{} := {}", name, expr))),
         _ => todo!("Not supported (yet). wp"),
     }
 }
